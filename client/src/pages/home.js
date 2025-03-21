7f46b58005d5afacbc46eb7c6e9534024254432c
Jack Anderson <jack.w.anderson2020@gmail.com>
2025-03-21 01:50:10 -0500

    started work on home page for athletes, database call to get data and plugged in goal component for active goals

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
 import TopBar from "../components/TopBar";
-
+import { jwtDecode } from "jwt-decode";
+import Goal from "../components/Goal";
 
 export default function home() {
     const router = useRouter();
+
+    const [profileData, setProfileData] = useState(null);
+    const [accType, setAccType] = useState(null)
 
     const handleLogout = () => {
         if(typeof window != 'undefined') {
@@ -14,13 +18,51 @@ export default function home() {
         router.push("/login");
     }
 
+    useEffect(() => {
+        if (typeof window != 'undefined') {
+            const token = localStorage.getItem('jwt');
+            const decodedToken = jwtDecode(token);
+            setAccType(decodedToken.accType);
+        }
+        const getHomePageInfo = async () => {
+            try {
+                const token = localStorage.getItem('jwt');
+                const response = await fetch("http://localhost:3001/api/home", {
+                    method: "GET",
+                    headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"},
+                });
+                const status = await response.status;
+                if(status === '401') {
+                    router.push("/login");
+                }
+
+                const data = await response.json();
+                console.log(data);
+                setProfileData(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getHomePageInfo();
+        console.log(profileData);
+    }, []);
+
     return (
         <div className="flex min-h-screen">
             <Navbar />
             <div className="flex flex-col flex-1 md:ml-64">
                 <TopBar pageName={"Home"} onLogout={handleLogout}/>
                 <div className="flex-1 p-4 bg-white">
+                    {accType && accType === 'athlete' && (
+                        <>
+                            <h2 className="text-gray-600 font-bebas-neue text-4xl underline px-6 pt-2 tracking-wider">Active Goals</h2>
+                            <div className="flex flex-col flex-1 ml-6 pt-1 xl:grid xl:grid-cols-3">
+                                {profileData && profileData.data.goals[0] && (<Goal goals={profileData.data.goals} />)}
+                            </div>
 
+                            <h2 className="text-gray-600 font-bebas-neue text-4xl underline px-6 pt-2 tracking-wider">Most Recent Report</h2>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
